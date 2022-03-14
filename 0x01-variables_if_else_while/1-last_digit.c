21d7e291a9e1fc8d6b65c632da2ff662332520f7
John Paul <johnpaul@trees.local>
2022-03-14 14:31:18 +0300

    format code 1 and introduced p

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, p);
 }
 else if (p == 0)
 {
-printf("Last digit of %d is %d and is greater than 0\n", n, p);
+printf("Last digit of %d is %d and is 0\n", n, p);
 /* checks if no is negative */
 }
 else
