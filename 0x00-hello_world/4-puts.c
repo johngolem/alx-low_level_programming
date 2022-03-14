f7088c6abbd6a859bdcb7fb6b24bd45457e978ec
John Paul <johnpaul@trees.local>
2022-03-15 00:29:25 +0300

    using puts to print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-char txt[]="Programming is like building a multilingual puzzle";
-puts(txt);
-return(0);
+puts= ("\"Programming is like building a multilingual puzzle");
+return (0);
 }
