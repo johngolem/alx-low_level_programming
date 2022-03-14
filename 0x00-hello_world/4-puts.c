47934ad1a103d3076348096912099c8dbc8a14b5
John Paul <johnpaul@trees.local>
2022-03-15 00:31:52 +0300

    using puts to print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * using ASCII
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* first chac to be written*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
