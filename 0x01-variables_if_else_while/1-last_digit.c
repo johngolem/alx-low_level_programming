cad608013fa3a6c59ce4fb4ce34381e8ba50ccdd
John Paul <johnpaul@trees.local>
2022-03-14 14:28:05 +0300

    format code 1 and introduced p

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,23 +10,25 @@
 int main(void)
 {
 int n;
+int p;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+p = (n%10);
 /* your code goes there */
-if ((n % 10) > 5)
+if (p > 5)
 {
 /* checks if no if positive */
-printf("Last digit of %d is %d and is greater than 5\n", n, n);
+printf("Last digit of %d is %d and is greater than 5\n", n, p);
 /* checks if no is zero */
 }
-else if ((n % 10) == 0)
+else if (p == 0)
 {
-printf("Last digit of %d is %d and is greater than 0\n", n, n);
+printf("Last digit of %d is %d and is greater than 0\n", n, p);
 /* checks if no is negative */
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 or not 0\n", n, n);
+printf("Last digit of %d is %d and is less than 6 or not 0\n", n, p);
 }
 return (0);
 }
