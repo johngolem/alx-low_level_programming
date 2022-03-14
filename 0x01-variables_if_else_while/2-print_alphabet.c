aa58c0657f931918aee79a21a3471ebe3fbdfe62
John Paul <johnpaul@trees.local>
2022-03-14 15:33:55 +0300

    print alphaet in lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-int main()
+/* main function to get characters
+* using ASCII */
+int main(void)
 {
 /* first chac to be written*/
 char ch = 'a';
 
 /*chac to be written in stdout*/
 for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
+putchar(ch \n);
 return (0);
 }
