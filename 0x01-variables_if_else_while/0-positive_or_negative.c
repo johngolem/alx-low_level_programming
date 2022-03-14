2169286b94732ebd1bacb9fb0662e027040d962a
John Paul <johnpaul@trees.local>
2022-03-14 13:25:11 +0300

    update 0-posi

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,16 +8,13 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if(n > 0)
-{
-printf(" is positive\n");
+if(n > 0) {
+		printf("%d is positive\n", n);
 } else if (n == 0){
-printf("n is zero\n");
+		printf("%d is zero\n", n);
 } else {
-printf("n is negative\n");
+		printf("%d is negative\n", n);
 }
 return (0);
 }
-
-
 
