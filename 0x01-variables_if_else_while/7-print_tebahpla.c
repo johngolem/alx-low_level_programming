26449cb0ae50dd8608c07dd4b81cc56d67d01e67
John Paul <johnpaul@trees.local>
2022-03-14 23:47:06 +0300

    print from a to z in reversed order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * using ASCII
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'z';
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+}
