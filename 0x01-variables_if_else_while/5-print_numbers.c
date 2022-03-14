e14a8da287cce74f7c7f1c996838aa45f400ee28
John Paul <johnpaul@trees.local>
2022-03-14 23:17:25 +0300

    print from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * using ASCII
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x = 0;
+for (x = 0; x < 10; x++)
+{
+printf("%d \n", x);
+}
+}
