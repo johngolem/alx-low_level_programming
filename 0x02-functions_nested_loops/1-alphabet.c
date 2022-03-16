855004c5fcf8debb07fc3fb1cd03cb495c1c64d9
John Paul <johnpaul@trees.local>
2022-03-16 17:51:16 +0300

    add the prototypes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 /**
  * main - entry point
@@ -6,33 +7,17 @@
  *
  * Return: Always 0 (Success)
  */
-/*void print_alphabet(void)
+int main(void)
+
 {
 /* first chac to be written*/
-/*char ch = 'a';
+char ch = 'a';
 
-/*chac to be written in stdout
+/*chac to be written in stdout*/
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
-putchar('\n');*/
-
-#include <stdio.h>
-#include "main.h"
-
-/**
- * print_alphabet - secondary function
- *
- * Description: prints each letter of the alphabet, ending with a newline
- */
-void print_alphabet(void)
-{
-char abc[] = "abcdefghijklmnopqrstuvwxyz\n";
-int i;
-
-for (i = 0; i < 27; i++)
-{
-    _putchar(abc[i]);
-}
+putchar('\n');
+return (0);
 }
